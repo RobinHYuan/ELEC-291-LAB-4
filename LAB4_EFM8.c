bc56b4aa97a040023d565e22e41eb7a3753a3372
RobinHYuan <68177491+RobinHYuan@users.noreply.github.com>
2021-02-28 17:06:52 -0800

    Add files via upload

---
+++ LAB4_EFM8.c
@@ -0,0 +1,36 @@
+// ADC.c:  Shows how to use the 14-bit ADC.  This program
+// measures the voltage from some pins of the EFM8LB1 using the ADC.
+//
+// (c) 2008-2018, Jesus Calvino-Fraga
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <EFM8LB1.h>
+#include <lab4.h>
+#define TRUE 1
+void main (void)
+{
+	
+	float voltage=0, tempCelsius=0;
+
+    waitms(500); // Give PuTTy a chance to start before sending
+	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
+	printf ("ADC test program\n"
+	        "File: %s\n"
+	        "Compiled: %s, %s\n\n",
+	        __FILE__, __DATE__, __TIME__);
+	
+	InitPinADC(2, 2); // Configure P2.2 as analog input
+    InitADC();
+
+	while(TRUE)
+	{
+		voltage = Volts_at_Pin(QFP32_MUX_P2_2);
+		tempCelsius = 100.0*(voltage-2.73);
+		printf ("Temperature=%.3f  \r\n", tempCelsius);
+		waitms(500);
+	 }  
+	 
+}	
+
